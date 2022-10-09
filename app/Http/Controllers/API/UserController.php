aae3c3586c70e0a586f5897874aadf3cf4985279
ErikROCHA <okerik.com@gmail.com>
2022-10-09 17:58:05 -0500

    users.index

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -10,14 +10,31 @@ class UserController extends Controller
 {
     public function index()
     {
-        echo "index";
+        $users = User::select('*')->get();
+        return response()->json([
+            'count' => count($users),
+            'items' => $users
+        ]);
     }
 
     public function store(Request $request)
     {
-        return User::create([
-            'token_device' => $request['token_device'],
-            'status' => 1
-        ]);
+        $token_receive = $request['token_device'];
+
+        $tokenExist = User::select('*')
+            ->where('users.token_device', '=', $request->input('token_device'))
+            ->get();
+        
+        if (count($tokenExist) == 0) {
+            return User::create([
+                'token_device' => $request['token_device'],
+                'status' => 1
+            ]);
+        } else {
+            return response()->json([
+                'msg' => 'Este dispositivo ya ha sido registrado'
+            ]);
+        }
+        
     }
 }
