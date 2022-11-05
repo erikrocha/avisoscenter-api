ff927b431f2d09004f7488d28b5af9a1b3225334
ErikROCHA <okerik.com@gmail.com>
2022-11-05 00:37:50 -0500

    fix searchAdCategories ads.created_at as datetime

--- app/Http/Controllers/API/AdController.php
+++ app/Http/Controllers/API/AdController.php
@@ -45,12 +45,11 @@ class AdController extends Controller
 
     public function searchAdCategories(Request $request)
     {
-        $ad_id = $request->input('ad_id');
-        $sac = AdCategory::select('*')
+        $sac = AdCategory::select('*',  'ads.created_at as datetime')
             ->join('ads', 'ads.id', '=', 'ad_categories.ad_id')
             ->join('categories', 'categories.id', '=', 'ad_categories.category_id')
             // ->orderByDesc('documents.created_at')
-            ->where('ad_id', '=', $ad_id)
+            ->where('ad_id', '=', $request->input('ad_id'))
             ->get();
         
         return response()->json([
