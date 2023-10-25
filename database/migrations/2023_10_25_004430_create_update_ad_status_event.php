a27aa919486b22b6acdad5472bdb182b585c1c0c
ErikROCHA <okerik.com@gmail.com>
2023-10-25 17:12:36 -0500

    ad expired to status = 0

---
+++ database/migrations/2023_10_25_004430_create_update_ad_status_event.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared('
+            CREATE EVENT UpdateAdStatus
+            ON SCHEDULE EVERY 1 HOUR
+            DO
+              UPDATE ads
+              SET status = 0
+              WHERE expired_at <= NOW();
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::unprepared('DROP EVENT IF EXISTS UpdateAdStatus');
+    }
+};
